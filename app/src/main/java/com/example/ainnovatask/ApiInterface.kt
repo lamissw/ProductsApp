8025d23577b4f8c18dfcbc13ff683d014e25fc92
Lamis Sweidan <89479223+lamissw@users.noreply.github.com>
2024-03-25 23:40:38 +0300

    create data class for product and call the api endpoint in api interface

--- app/src/main/java/com/example/ainnovatask/ApiInterface.kt
+++ app/src/main/java/com/example/ainnovatask/ApiInterface.kt
@@ -1,10 +1,15 @@
+import com.example.ainnovatask.Product
 import com.example.ainnovatask.UserRequest
 import com.example.ainnovatask.UserResponse
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
+import retrofit2.http.GET
 
 interface ApiInterface {
     @POST("auth/login")
     fun requestLogin(@Body dataModel: UserRequest?): Call<UserResponse>
+
+    @GET("products")
+    fun getProducts() : Call<List<Product>>
 }
