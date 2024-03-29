7e625f2713aead298b57f63dbe16e5ba638b30cc
Lamis Sweidan <89479223+lamissw@users.noreply.github.com>
2024-03-29 18:59:02 +0300

    all products are displayed as needed product screen UI is done TODO-> open product details screen and make image circular

--- app/src/main/java/com/example/ainnovatask/ProductAdapter.kt
+++ app/src/main/java/com/example/ainnovatask/ProductAdapter.kt
@@ -4,13 +4,18 @@ import android.view.LayoutInflater
 import androidx.recyclerview.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
+
 
 class ProductAdapter(private val productList: List<Product>) :
     RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
 
     inner class ProductViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val productName = itemView.findViewById<TextView>(R.id.product_title)
+        val productThumbnail = itemView.findViewById<ImageView>(R.id.product_thumbnail)
+        val productDescription = itemView.findViewById<TextView>(R.id.product_description)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
@@ -22,6 +27,8 @@ class ProductAdapter(private val productList: List<Product>) :
     override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
         val product = productList[position]
         holder.productName.text = product.title
+        holder.productDescription.text = product.description
+        Glide.with(holder.itemView).load(product.thumbnail).into(holder.productThumbnail)
     }
 
     override fun getItemCount() = productList.size
