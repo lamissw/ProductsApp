468695651abae76692e7b6c99a8a7c8badde3f7c
Lamis Sweidan <89479223+lamissw@users.noreply.github.com>
2024-03-30 17:57:31 +0300

    final ui done

--- app/src/main/java/com/example/ainnovatask/ProductDetailsActivity.kt
+++ app/src/main/java/com/example/ainnovatask/ProductDetailsActivity.kt
@@ -5,14 +5,12 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import com.bumptech.glide.Glide
-import org.w3c.dom.Text
-import android.content.Intent
 
 class ProductDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.product_details_screen)
-//
+
         val thumbnail = intent.getStringExtra("product_thumbnail")
         val title = intent.getStringExtra("product_title")
         val description = intent.getStringExtra("product_description")
@@ -20,7 +18,6 @@ class ProductDetailsActivity : AppCompatActivity() {
         val category = intent.getStringExtra("product_category")
         val price = intent.getDoubleExtra("product_price", 0.0)
         val rating = intent.getDoubleExtra("product_rating", 0.0)
-//
         val productThumbnail = findViewById<ImageView>(R.id.product_thumbnail)
         val productTitle = findViewById<TextView>(R.id.product_title)
         val productDescription = findViewById<TextView>(R.id.product_description)
@@ -28,7 +25,7 @@ class ProductDetailsActivity : AppCompatActivity() {
         val productCategory = findViewById<TextView>(R.id.product_category)
         val productPrice = findViewById<TextView>(R.id.product_price)
         val productRating = findViewById<TextView>(R.id.product_rating)
-//
+
         Glide.with(this).load(thumbnail).into(productThumbnail)
         productTitle.text = title
         productDescription.text = description
